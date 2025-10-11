91b5919620b18022f6727d296052e4fb7afccf71
sakshisewatkar <136146512+sakshisewatkar@users.noreply.github.com>
2025-10-11 22:38:18 +0530

    add repositroy

--- src/main/java/com/LifeLineHospital/Hospital/Controller/PatientController.java
+++ src/main/java/com/LifeLineHospital/Hospital/Controller/PatientController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.LifeLineHospital.Hospital.Model.Patient;
+
 
 @RestController
 public class PatientController {
