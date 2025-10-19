842596120d14632eb1aa1758bc78bb2850e1336d
sakshisewatkar <136146512+sakshisewatkar@users.noreply.github.com>
2025-10-19 22:55:19 +0530

    add patch method

--- src/main/java/com/LifeLineHospital/Hospital/Controller/PatientController.java
+++ src/main/java/com/LifeLineHospital/Hospital/Controller/PatientController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -29,25 +30,32 @@ public class PatientController {
 		return "Welcome to LIFELINE Hospital";
 	}
 	
-
+//PostMethod
 @PostMapping("/add")
 public Patient addpatient (@RequestBody Patient patient) {
 	return patientServiceIMPL.savePatient(patient);
 }
-
+//Get method
 @GetMapping("/all")
 public List<Patient> getAllPatient(){
 	return patientServiceIMPL.getallpatient();
 }
+//Put Method
 @PutMapping("/update")
 public Patient updatedPatient(@RequestBody Patient patient) {
 	return patientServiceIMPL.updatepatient(patient);
 }
-
+//Delete Method
 @DeleteMapping("/delete/{id}")
 public String deletepatient (@PathVariable Integer id) {
 	return patientServiceIMPL.deletepatient(id);
 }
+
+@PatchMapping("/updateName/{id}")
+public Patient updateName (@PathVariable Integer id , @RequestBody String name) {
+	return patientServiceIMPL.updateName(name, id);
+}
+
 	
 
 }
