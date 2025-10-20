c4b2465c65e755e30ed1ce1dc300d563b6a42ed4
sakshisewatkar <136146512+sakshisewatkar@users.noreply.github.com>
2025-10-20 22:18:33 +0530

    addDETAILSAppoinment

--- src/main/java/com/LifeLineHospital/Hospital/Controller/AppointmentController.java
+++ src/main/java/com/LifeLineHospital/Hospital/Controller/AppointmentController.java
@@ -3,11 +3,20 @@ package com.LifeLineHospital.Hospital.Controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.LifeLineHospital.Hospital.Model.Appointment;
+import com.LifeLineHospital.Hospital.sServiceIMPL.AppoinmentServiceIMPL;
+
 @RestController
+@RequestMapping("/appoinment")
 public class AppointmentController {
+	@Autowired AppoinmentServiceIMPL appoinmentServiceIMPL;
 	@GetMapping("/AllAppointment")
 	public List<String> GetAppointment()
 	{
@@ -17,5 +26,10 @@ public class AppointmentController {
 				  "Appointment 3: 2025-10-09 02:00 PM");
 				
 	}
+	
+	@PostMapping("/add")
+public Appointment addAppoinment (@RequestBody Appointment appointment) {
+	return appoinmentServiceIMPL.saveAppoinment(appointment);
+}
 
 }
--- src/main/java/com/LifeLineHospital/Hospital/sServiceIMPL/AppoinmentServiceIMPL.java
+++ src/main/java/com/LifeLineHospital/Hospital/sServiceIMPL/AppoinmentServiceIMPL.java
@@ -1,5 +1,24 @@
 package com.LifeLineHospital.Hospital.sServiceIMPL;
 
-public class AppoinmentServiceIMPL {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.LifeLineHospital.Hospital.Model.Appointment;
+import com.LifeLineHospital.Hospital.Repository.AppoinmentRepository;
+import com.LifeLineHospital.Hospital.Service.AppoinmentService;
+
+@Service
+public class AppoinmentServiceIMPL implements AppoinmentService {
+	
+	@Autowired
+	private AppoinmentRepository appoinmentRepository;
+
+	@Override
+	public Appointment saveAppoinment(Appointment appointment) {
+		return appoinmentRepository.save(appointment);
+	}
+	
+	
+
 
 }
